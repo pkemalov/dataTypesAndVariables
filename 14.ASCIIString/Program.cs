4b9c202137181cd8999f2929eedf4f76b86c9074
pkemalov <petar.kemalov@gmail.com>
2017-02-06 20:29:35 +0200

    commited

---
+++ 14.ASCIIString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.ASCIIString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            String result = "";
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                char a = (char)number;
+                result += a.ToString();
+                                
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
